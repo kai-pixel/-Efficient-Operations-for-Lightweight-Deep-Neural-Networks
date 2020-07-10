4b28de85f93112360cb24f68f12c8ef023426be7
kai-pixel <64736109+kai-pixel@users.noreply.github.com>
2020-07-10 18:37:12 +0200

    Create main.py
    one file for train and validation

---
+++ Cifar100/main.py
@@ -0,0 +1,438 @@
+import os
+import sys
+
+import argparse
+from datetime import datetime
+import torch.backends.cudnn as cudnn
+
+import time
+import math
+import torch.nn.init as init
+from datetime import datetime
+from torch.optim.lr_scheduler import _LRScheduler
+
+import pickle
+#from skimage import io
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import numpy
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.autograd import Variable
+
+from tensorboardX import SummaryWriter
+
+
+
+################################################################################################################################## global setting
+
+#mean and std of cifar100 dataset
+CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
+CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
+
+#CIFAR100_TEST_MEAN = (0.5088964127604166, 0.48739301317401956, 0.44194221124387256)
+#CIFAR100_TEST_STD = (0.2682515741720801, 0.2573637364478126, 0.2770957707973042)
+
+#directory to save weights file
+CHECKPOINT_PATH = 'your/path/to/checkpoint'
+
+#total training epoches
+EPOCH = 300 
+MILESTONES = [60, 120, 160]
+
+#initial learning rate
+#INIT_LR = 0.1
+
+#time of we run the script
+TIME_NOW = datetime.now().isoformat()
+
+#tensorboard log dir
+LOG_DIR = 'your/path/to/runs'
+
+#save weights file per SAVE_EPOCH epoch
+SAVE_EPOCH = 10
+
+###############################################################################################################################################utils(1)
+def get_network(args, use_gpu=True):
+    if args.net == 'MobileNetV3':
+        from models.MobileNetV3 import MobileNetV3
+        net = MobileNetV3()        
+        
+    elif args.net == 'v3_x_sereuse':
+        from models.v3_x_sereuse import v3_x_sereuse
+        net = v3_x_sereuse()
+
+    else:
+        print('the network name you have entered is not supported yet')
+        sys.exit()
+    
+    if use_gpu:
+        net = net.cuda()
+
+    return net
+
+
+def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
+
+    transform_train = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    #cifar100_training = CIFAR100Train(path, transform=transform_train)
+    cifar100_training = torchvision.datasets.CIFAR100(root='/pytorch-cifar100/data', train=True, download=True, transform=transform_train)
+    cifar100_training_loader = DataLoader(
+        cifar100_training, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar100_training_loader
+
+def get_test_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
+ 
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    #cifar100_test = CIFAR100Test(path, transform=transform_test)
+    cifar100_test = torchvision.datasets.CIFAR100(root='/pytorch-cifar100/data', train=False, download=True, transform=transform_test)
+    cifar100_test_loader = DataLoader(
+        cifar100_test, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar100_test_loader
+
+def compute_mean_std(cifar100_dataset):
+ 
+
+    data_r = numpy.dstack([cifar100_dataset[i][1][:, :, 0] for i in range(len(cifar100_dataset))])
+    data_g = numpy.dstack([cifar100_dataset[i][1][:, :, 1] for i in range(len(cifar100_dataset))])
+    data_b = numpy.dstack([cifar100_dataset[i][1][:, :, 2] for i in range(len(cifar100_dataset))])
+    mean = numpy.mean(data_r), numpy.mean(data_g), numpy.mean(data_b)
+    std = numpy.std(data_r), numpy.std(data_g), numpy.std(data_b)
+
+    return mean, std
+
+class WarmUpLR(_LRScheduler):
+
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+        
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """we will use the first m batches, and set the learning
+        rate to base_lr * m / total_iters
+        """
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+
+#############################################################################################################################################utils(2)
+
+class CIFAR100Train(Dataset):
+
+    def __init__(self, path, transform=None):
+        #if transform is given, we transoform data using
+        with open(os.path.join(path, 'train'), 'rb') as cifar100:
+            self.data = pickle.load(cifar100, encoding='bytes')
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.data['fine_labels'.encode()])
+
+    def __getitem__(self, index):
+        label = self.data['fine_labels'.encode()][index]
+        r = self.data['data'.encode()][index, :1024].reshape(32, 32)
+        g = self.data['data'.encode()][index, 1024:2048].reshape(32, 32)
+        b = self.data['data'.encode()][index, 2048:].reshape(32, 32)
+        image = numpy.dstack((r, g, b))
+
+        if self.transform:
+            image = self.transform(image)
+        return label, image
+
+class CIFAR100Test(Dataset):
+
+    def __init__(self, path, transform=None):
+        with open(os.path.join(path, 'test'), 'rb') as cifar100:
+            self.data = pickle.load(cifar100, encoding='bytes')
+        self.transform = transform 
+
+    def __len__(self):
+        return len(self.data['data'.encode()])
+    
+    def __getitem__(self, index):
+        label = self.data['fine_labels'.encode()][index]
+        r = self.data['data'.encode()][index, :1024].reshape(32, 32)
+        g = self.data['data'.encode()][index, 1024:2048].reshape(32, 32)
+        b = self.data['data'.encode()][index, 2048:].reshape(32, 32)
+        image = numpy.dstack((r, g, b))
+
+        if self.transform:
+            image = self.transform(image)
+        return label, image
+    
+##################################################################################################################################################utils(3)
+
+def get_mean_and_std(dataset):
+    '''Compute the mean and std value of dataset.'''
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    print('==> Computing mean and std..')
+    for inputs, targets in dataloader:
+        for i in range(3):
+            mean[i] += inputs[:,i,:,:].mean()
+            std[i] += inputs[:,i,:,:].std()
+    mean.div_(len(dataset))
+    std.div_(len(dataset))
+    return mean, std
+
+def init_params(net):
+    '''Init layer parameters.'''
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            init.kaiming_normal(m.weight, mode='fan_out')
+            if m.bias:
+                init.constant(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            init.constant(m.weight, 1)
+            init.constant(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            init.normal(m.weight, std=1e-3)
+            if m.bias:
+                init.constant(m.bias, 0)
+
+
+
+term_width = 80
+
+TOTAL_BAR_LENGTH = 65.
+last_time = time.time()
+begin_time = last_time
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
+
+############################################################################################################################################### Train and Val
+def train(epoch):
+
+    net.train()
+    for batch_index, (images, labels) in enumerate(cifar100_training_loader):
+        if epoch <= args.warm:
+            warmup_scheduler.step()
+
+        images = Variable(images)
+        labels = Variable(labels)
+
+        labels = labels.cuda()
+        images = images.cuda()
+
+        optimizer.zero_grad()
+        outputs = net(images)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
+
+        #update training loss for each iteration
+        writer.add_scalar('Train/loss', loss.item(), n_iter)
+
+def eval_training(epoch):
+    net.eval()
+
+    test_loss = 0.0 # cost function error
+    correct = 0.0
+
+    for (images, labels) in cifar100_test_loader:
+        images = Variable(images)
+        labels = Variable(labels)
+
+        images = images.cuda()
+        labels = labels.cuda()
+
+        outputs = net(images)
+        loss = loss_function(outputs, labels)
+        test_loss += loss.item()
+        _, preds = outputs.max(1)
+        correct += preds.eq(labels).sum()
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {:.4f}'.format(
+        test_loss / len(cifar100_test_loader.dataset),
+        correct.float() / len(cifar100_test_loader.dataset)
+    ))
+    print()
+
+    #add informations to tensorboard
+    writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
+    writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
+
+    return correct.float() / len(cifar100_test_loader.dataset)
+######################################################################################################################################## main
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-net', type=str, default='MobileNetV3', required=True, help='net type')
+    parser.add_argument('-gpu', type=bool, default=True, help='use gpu or not')
+    parser.add_argument('-w', type=int, default=2, help='number of workers for dataloader')
+    parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
+    parser.add_argument('-s', type=bool, default=True, help='whether shuffle the dataset')
+    parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
+    parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
+    args = parser.parse_args()
+    
+    
+ ################################# #data preprocessing:
+
+    cifar100_training_loader = get_training_dataloader(
+        CIFAR100_TRAIN_MEAN,
+        CIFAR100_TRAIN_STD,
+        num_workers=args.w,
+        batch_size=args.b,
+        shuffle=args.s
+    )
+    
+    cifar100_test_loader = get_test_dataloader(
+        CIFAR100_TRAIN_MEAN,
+        CIFAR100_TRAIN_STD,
+        num_workers=args.w,
+        batch_size=args.b,
+        shuffle=args.s
+    )
+
+################################# gpu or cpu
+
+
+    net = get_network(args, use_gpu=args.gpu)
+    print(net)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+
+        
+
+################################## multi gpu
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net) # make parallel
+        cudnn.benchmark = True
+    
+    loss_function = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=MILESTONES, gamma=0.2) #learning rate decay
+    iter_per_epoch = len(cifar100_training_loader)
+
+    warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
+    checkpoint_path = os.path.join(CHECKPOINT_PATH, args.net, TIME_NOW)
+
+################################### use tensorboard to save log
+    if not os.path.exists(LOG_DIR):
+        os.mkdir(LOG_DIR)
+    writer = SummaryWriter(LOG_DIR,args.net)
+    
+################################### create checkpoint folder to save model
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
+
+    best_acc = 0.0
+    for epoch in range(1, EPOCH):
+        if epoch > args.warm:
+            train_scheduler.step(epoch)
+
+        train(epoch)
+        acc = eval_training(epoch)
+
+        #start to save best performance model after learning rate decay to 0.01 
+        if epoch > MILESTONES[1] and best_acc < acc:
+            torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='best'))
+            best_acc = acc
+            continue
+
+        if not epoch % SAVE_EPOCH:
+            torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='regular'))
+
+    writer.close()
