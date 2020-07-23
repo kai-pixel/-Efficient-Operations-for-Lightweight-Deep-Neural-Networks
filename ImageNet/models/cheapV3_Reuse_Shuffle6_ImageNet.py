739abaa9d872084d80aa401b7923872e67c5315e
kai-pixel <64736109+kai-pixel@users.noreply.github.com>
2020-07-23 13:54:04 +0200

    Update cheapV3_Reuse_Shuffle6_ImageNet.py

--- ImageNet/models/cheapV3_Reuse_Shuffle6_ImageNet.py
+++ ImageNet/models/cheapV3_Reuse_Shuffle6_ImageNet.py
@@ -261,14 +261,12 @@ class cheapV3_Reuse_Shuffle6_ImageNet(nn.Module):
         last_channels_num = _ensure_divisible(last_channels_num * width_multiplier, divisor) if width_multiplier > 1 else last_channels_num
         feature_extraction_layers = []
         first_layer = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=3, momentum=BN_momentum),
             reuse4(),
-            reuse4(),
-            nn.Conv2d(in_channels=48, out_channels=48, kernel_size=3, stride=s, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=48, momentum=BN_momentum),
-            nn.Conv2d(in_channels=48, out_channels=input_channels_num, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(num_features=input_channels_num, momentum=BN_momentum),
+            reuse2(),
+            nn.Conv2d(in_channels=24, out_channels=24, kernel_size=3, stride=s, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=24, momentum=BN_momentum),
+            #nn.Conv2d(in_channels=48, out_channels=input_channels_num, kernel_size=1, stride=1, padding=0, bias=False),
+            #nn.BatchNorm2d(num_features=input_channels_num, momentum=BN_momentum),
             H_swish()
         )
         feature_extraction_layers.append(first_layer)
