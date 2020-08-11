a18bd854b7438fc9a16ca5f6cdc77c5ad1100f06
kai-pixel <64736109+kai-pixel@users.noreply.github.com>
2020-08-11 16:08:59 +0200

    Update model.py

--- group conv/model.py
+++ group conv/model.py
@@ -1,1 +1,370 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from collections import OrderedDict
 
+from thop import profile
+from torchsummary import summary
+from flopth import flopth
+
+
+def channel_shuffle(x, groups):
+    batchsize, num_channels, height, width = x.data.size()
+
+    channels_per_group = num_channels // groups
+    
+    # reshape
+    x = x.view(batchsize, groups, 
+        channels_per_group, height, width)
+
+    # transpose
+    x = torch.transpose(x, 1, 2).contiguous()
+
+    # flatten
+    x = x.view(batchsize, -1, height, width)
+
+    return x
+
+
+
+
+def _ensure_divisible(number, divisor, min_value=None):
+    '''
+    Ensure that 'number' can be 'divisor' divisible
+    '''
+    if min_value is None:
+        min_value = divisor
+    new_num = max(min_value, int(number + divisor / 2) // divisor * divisor)
+    if new_num < 0.9 * number:
+        new_num += divisor
+    return new_num
+
+class H_sigmoid(nn.Module):
+    '''
+    hard sigmoid
+    '''
+    def __init__(self, inplace=True):
+        super(H_sigmoid, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, x):
+        return F.relu6(x + 3, inplace=self.inplace) / 6
+
+class H_swish(nn.Module):
+    '''
+    hard swish
+    '''
+    def __init__(self, inplace=True):
+        super(H_swish, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, x):
+        return x * F.relu6(x + 3, inplace=self.inplace) / 6
+
+class SEModule(nn.Module):
+    '''
+    SE Module
+    Ref: https://github.com/moskomule/senet.pytorch/blob/master/senet/se_module.py
+    '''
+    def __init__(self, in_channels_num, reduction_ratio=8):
+        super(SEModule, self).__init__()
+
+        if in_channels_num % reduction_ratio != 0:
+            raise ValueError('in_channels_num must be divisible by reduction_ratio(default = 4)')
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels_num, in_channels_num // reduction_ratio, bias=False),
+            nn.ReLU(inplace=True),
+            reuse4(),
+            reuse2(),
+            H_sigmoid()
+        )
+
+    def forward(self, x):
+        batch_size, channel_num, _, _ = x.size()
+        y = self.avg_pool(x).view(batch_size, channel_num)
+        y = self.fc(y).view(batch_size, channel_num, 1, 1)
+        return x * y
+
+class reuse6A(nn.Module):
+   def __init__(self,in_channels_num):
+       super(reuse6A,self).__init__()
+
+   def forward(self, x):
+     batch_size, channel_num, _, _ = x.size()
+     x = channel_shuffle(x,6)
+     out = torch.cat((x,x,x,x,x,x),dim=1)
+     return out
+
+class reuse6(nn.Module):
+   def __init__(self):
+       super(reuse6,self).__init__()
+
+   def forward(self, x):
+     out = torch.cat((x,x,x,x,x,x),dim=1)
+     return out
+
+class reuse4(nn.Module):
+   def __init__(self):
+       super(reuse4,self).__init__()
+
+   def forward(self, x):
+     out = torch.cat((x,x,x,x),dim=1)
+     return out
+
+class reuse2(nn.Module):
+   def __init__(self):
+       super(reuse2,self).__init__()
+
+   def forward(self, x):
+     out = torch.cat((x,x),dim=1)
+     return out
+
+
+class gconv(nn.Module):
+   def __init__(self, Block_num, exp_size, kernel_size, stride, BN_momentum):
+       super(gconv,self).__init__()
+       self.Block_num = Block_num
+
+       self.conv = nn.Conv2d(in_channels=exp_size, out_channels=exp_size, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, groups=exp_size, bias=False)
+       self.bn = nn.BatchNorm2d(num_features=exp_size, momentum=BN_momentum)
+       # groups=exp_size//2 or groups=exp_size//3
+       self.conv1 = nn.Conv2d(in_channels=exp_size, out_channels=exp_size, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, groups=exp_size//2, bias=False)
+       self.bn1 = nn.BatchNorm2d(num_features=exp_size, momentum=BN_momentum)
+
+   def forward(self, x):
+     if self.Block_num==1 or self.Block_num==2 or self.Block_num==3 or self.Block_num==4 or self.Block_num==5 or self.Block_num==6 or self.Block_num==7:
+       out1 = self.bn(self.conv(x))
+       return out1
+     else:
+       out2 = self.bn1(self.conv1(x)) 
+       return out2
+    
+     return out
+
+class Bottleneck(nn.Module):
+    '''
+    The basic unit of MobileNetV3
+    '''
+    def __init__(self, Block_num, in_channels_num, exp_size, out_channels_num, kernel_size, stride, use_SE, NL, BN_momentum):
+        '''
+        use_SE: True or False -- use SE Module or not
+        NL: nonlinearity, 'RE' or 'HS'
+        '''
+        super(Bottleneck, self).__init__()
+
+        assert stride in [1, 2]
+        NL = NL.upper()
+        assert NL in ['RE', 'HS']
+
+        use_HS = NL == 'HS'
+        
+        # Whether to use residual structure or not
+        self.use_residual = (stride == 1 and in_channels_num == out_channels_num)
+
+
+        if exp_size == in_channels_num:
+            # Without expansion, the first depthwise convolution is omitted
+            self.conv = nn.Sequential(
+                # Depthwise Convolution
+                nn.Conv2d(in_channels=in_channels_num, out_channels=exp_size, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, groups=in_channels_num, bias=False),
+                nn.BatchNorm2d(num_features=exp_size, momentum=BN_momentum),
+                # SE Module
+                SEModule(exp_size) if use_SE else nn.Sequential(),
+                H_swish() if use_HS else nn.ReLU(inplace=True),
+                # Linear Pointwise Convolution
+                nn.Conv2d(in_channels=exp_size, out_channels=out_channels_num, kernel_size=1, stride=1, padding=0, bias=False),
+                #nn.BatchNorm2d(num_features=out_channels_num, momentum=BN_momentum)
+                nn.Sequential(OrderedDict([('lastBN', nn.BatchNorm2d(num_features=out_channels_num))])) if self.use_residual else
+                    nn.BatchNorm2d(num_features=out_channels_num, momentum=BN_momentum)
+            )
+        else:
+            # With expansion
+            self.conv = nn.Sequential(
+                # Pointwise Convolution for expansion
+                #nn.Conv2d(in_channels=in_channels_num, out_channels=exp_size, kernel_size=1, stride=1, padding=0, bias=False),
+                #nn.BatchNorm2d(num_features=exp_size, momentum=BN_momentum),
+
+                reuse6A(in_channels_num),
+                H_swish() if use_HS else nn.ReLU(inplace=True),
+                # Depthwise Convolution
+                #nn.Conv2d(in_channels=exp_size, out_channels=exp_size, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, groups=exp_size, bias=False),
+                #nn.BatchNorm2d(num_features=exp_size, momentum=BN_momentum),
+                gconv(Block_num, exp_size, kernel_size, stride, BN_momentum),
+                # SE Module
+                SEModule(exp_size) if use_SE else nn.Sequential(),
+                H_swish() if use_HS else nn.ReLU(inplace=True),
+
+                # Linear Pointwise Convolution
+                nn.Conv2d(in_channels=exp_size, out_channels=out_channels_num, kernel_size=1, stride=1, padding=0, bias=False),
+                #nn.BatchNorm2d(num_features=out_channels_num, momentum=BN_momentum)
+                nn.Sequential(OrderedDict([('lastBN', nn.BatchNorm2d(num_features=out_channels_num))])) if self.use_residual else
+                    nn.BatchNorm2d(num_features=out_channels_num, momentum=BN_momentum)
+            )
+
+    def forward(self, x):
+        if self.use_residual:
+            return self.conv(x) + x
+        else:
+            return self.conv(x)
+
+
+class cheapV3G(nn.Module):
+    def __init__(self, mode='large', classes_num=10, input_size=32, width_multiplier=1.0, dropout=0.2, BN_momentum=0.1, zero_gamma=False):
+        '''
+        configs: setting of the model
+        mode: type of the model, 'large' or 'small'
+        '''
+        super(cheapV3G, self).__init__()
+
+        mode = mode.lower()
+        assert mode in ['large', 'small']
+        s = 2
+        if input_size == 32 or input_size == 56:
+            # using cifar-10, cifar-100 or Tiny-ImageNet
+            s = 1
+
+        # setting of the model
+        if mode == 'large':
+            # Configuration of a MobileNetV3-Large Model
+            configs = [
+                #Block_num, kernel_size, exp_size, out_channels_num, use_SE, NL, stride
+                [1, 3, 24, 24, False, 'RE', 1],
+                [2, 3, 144, 24, False, 'RE', s],
+                [3, 3, 144, 24, False, 'RE', 1],
+                [4, 5, 144, 48, True, 'RE', 2],
+                [5, 5, 288, 48, True, 'RE', 1],
+                [6, 5, 288, 48, True, 'RE', 1],
+                [7, 3, 288, 96, False, 'HS', 2],
+                [8, 3, 576, 96, False, 'HS', 1],
+                [9, 3, 576, 96, False, 'HS', 1],
+                [10, 3, 576, 96, False, 'HS', 1],
+                [11, 3, 576, 120, True, 'HS', 1],
+                [12, 3, 720, 120, True, 'HS', 1],
+                [13, 5, 720, 180, True, 'HS', 2],
+                [14, 5, 1080, 180, True, 'HS', 1],
+                [15, 5, 1080, 180, True, 'HS', 1]
+            ]
+        elif mode == 'small':
+            # Configuration of a MobileNetV3-Small Model
+            configs = [
+                #kernel_size, exp_size, out_channels_num, use_SE, NL, stride
+                [3, 16, 16, True, 'RE', s],
+                [3, 72, 24, False, 'RE', 2],
+                [3, 88, 24, False, 'RE', 1],
+                [5, 96, 40, True, 'HS', 2],
+                [5, 240, 40, True, 'HS', 1],
+                [5, 240, 40, True, 'HS', 1],
+                [5, 120, 48, True, 'HS', 1],
+                [5, 144, 48, True, 'HS', 1],
+                [5, 288, 96, True, 'HS', 2],
+                [5, 576, 96, True, 'HS', 1],
+                [5, 576, 96, True, 'HS', 1]
+            ]
+
+        first_channels_num = 24
+
+        # last_channels_num = 1280
+        # according to https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet_v3.py
+        # if small -- 1024, if large -- 1280
+        last_channels_num = 2160 if mode == 'large' else 1024
+
+        divisor = 4
+
+        ########################################################################################################################
+        # feature extraction part
+        # input layer
+        input_channels_num = _ensure_divisible(first_channels_num * width_multiplier, divisor)
+        last_channels_num = _ensure_divisible(last_channels_num * width_multiplier, divisor) if width_multiplier > 1 else last_channels_num
+        feature_extraction_layers = []
+        first_layer = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=s, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=3, momentum=BN_momentum),
+            reuse4(),
+            reuse4(),
+            nn.Conv2d(in_channels=48, out_channels=input_channels_num, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(num_features=input_channels_num, momentum=BN_momentum),
+            H_swish()
+        )
+        feature_extraction_layers.append(first_layer)
+        # Overlay of multiple bottleneck structures
+        for Block_num, kernel_size, exp_size, out_channels_num, use_SE, NL, stride in configs:
+            output_channels_num = _ensure_divisible(out_channels_num * width_multiplier, divisor)
+            exp_size = _ensure_divisible(exp_size * width_multiplier, divisor)
+            feature_extraction_layers.append(Bottleneck(Block_num, input_channels_num, exp_size, output_channels_num, kernel_size, stride, use_SE, NL, BN_momentum))
+            input_channels_num = output_channels_num
+        
+        # the last stage
+        last_stage_channels_num = _ensure_divisible(exp_size * width_multiplier, divisor)
+        last_stage_layer1 = nn.Sequential(
+                #nn.Conv2d(in_channels=input_channels_num, out_channels=last_stage_channels_num, kernel_size=1, stride=1, padding=0, bias=False),
+                #nn.BatchNorm2d(num_features=last_stage_channels_num, momentum=BN_momentum),
+                reuse6(),
+                H_swish()
+            )
+        feature_extraction_layers.append(last_stage_layer1)
+
+        # SE Module
+        # remove the last SE Module according to https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet_v3.py
+        # feature_extraction_layers.append(SEModule(last_stage_channels_num) if mode == 'small' else nn.Sequential())
+
+        feature_extraction_layers.append(nn.AdaptiveAvgPool2d(1))
+        feature_extraction_layers.append(reuse2())
+        feature_extraction_layers.append(H_swish())
+
+        self.features = nn.Sequential(*feature_extraction_layers)
+
+        ########################################################################################################################
+        # Classification part
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(last_channels_num, classes_num)
+        )
+
+        ########################################################################################################################
+        # Initialize the weights
+        self._initialize_weights(zero_gamma)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self, zero_gamma):
+        '''
+        Initialize the weights
+        '''
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+        if zero_gamma:
+            for m in self.modules():
+	            if hasattr(m, 'lastBN'):
+	                nn.init.constant_(m.lastBN.weight, 0.0)
+
+
+
+def test():
+    net = cheapV3G()
+    print(net)
+
+    x = torch.randn(2,3,224,224)
+    y = net(x)
+    print(y.size())
+    
+    net=net.cuda()
+    summary(net,input_size=(3, 224, 224))
+    print(flopth(net,in_size=(3,224,224)))
+#test()
